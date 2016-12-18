ee38815bb5fcc269ba5cfa4f272a77c02cbd0a4b
AMashoshyna <a.mashoshyna@gmail.com>
2016-12-18 12:25:04 +0200

    implement fetching questions

---
+++ script.js
@@ -0,0 +1,23 @@
+function req() {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://jservice.io/api/random', false);
+	xhr.send();
+
+	if(xhr.status !=200) {
+		console.log(xhr.status + ': ' + xhr.statusText)
+	} else {
+		const res = JSON.parse(xhr.responseText);
+		const question = res[0].question;
+		const answer = res[0].answer;
+		const id = res[0].id;
+		const category = res[0].category.title;
+
+
+	document.getElementById('question').innerHTML = question;
+	document.getElementById('id').innerHTML = 'QUESTION ' + id;
+	console.log(answer);
+	document.getElementById('category').innerHTML =  'Category: ' + category;
+	};
+
+};
+req();
